b19af7335085cd249087fdf5f1a5a5a404922fcd
SunilRupnoor <141404145+SunilRupnoor@users.noreply.github.com>
2023-08-04 14:26:46 +0530

    Update overloading.cpp

--- overloading.cpp
+++ overloading.cpp
@@ -1,77 +1,46 @@
-Design a C++ program to convert dollar to rupees, euro to rupees and pound to rupees using pure virtual functions.
-1 dollar = 54.3 Rs, 1 pound = 81.1Rs, 1 euro = 70Rs
+//Write a C++ program to perform ( ) overloading.
 #include<iostream>
 using namespace std;
-class rs
+class loc
 {
+  int longitude, latitude;
   public:
-  float rupees;
-  virtual void conv()=0;
-  void disp()
+  loc ( ) {}
+  loc(int lg, int lt)
   {
-    cout<<" is eqvivalent to "<<rupees<<" INR \n";
+    longitude = lg;
+    latitude = lt;
   }
+  void show ()
+  {
+    cout<< longitude<<" ";
+    cout<< latitude<<"\n ";
+  }
+  loc operator+ (loc op2);
+  loc operator()( int i, int j);
 };
-class doll:public rs
+// overload () for loc
+loc loc::operator()(int i, int j)
 {
-  float dol;
-  public:
-  void conv()
-  {
-    cout<<"Enter currncy in dollar \n";
-    cin>>dol;
-    rupees=54.3*dol;
-    cout<<" "<<dol<<" in dollar ";
-    disp();
-  }
-};
-class euro:public rs
+  longitude = i;
+  latitude = j;
+  return *this;
+}
+// overload + for loc
+loc loc::operator+(loc op2)
 {
-  float er;
-  public:
-  void conv()
-  {
-    cout<<"Enter currency in Euro \n";
-    cin>>er;
-    rupees=70.2*er;
-    cout<<" "<<er<<" in euro ";
-    disp();
-  }
-};
-class pd:public rs
-{
-  float pnd;
-  public:
-  void conv()
-  {
-    cout<<"Enter currency in pound \n";
-    cin>>pnd;
-    rupees=81.1*pnd;
-    cout<<" "<<pnd<<" in pound ";
-    disp();
-  }
-};
+  loc temp;
+  temp.longitude= op2.longitude+longitude;
+  temp.latitude= op2.latitude+latitude;
+  return temp;
+}
 int main()
 {
-  int c;
-  doll d;
-  euro e;
-  pd p;
-  cout<<"\t\t\t Currency conversion \n";
-  while(1)
-  {
-    cout<<"1.$ to Rs. 2.Euro to Rs. 3.Pound to Rs. 4.Exit \n";
-    cin>>c;
-    switch(c)
-    {
-        case 1:d.conv();
-        break;
-  case 2:e.conv();
-  break;
-  case 3:p.conv();
-  break;
-  default:return 0;
-  }
-}
-return 0;
+  loc ob1(10,20), ob2(1,1);
+  ob1.show();
+  ob1(7,8);
+  ob1.show();
+  ob1=ob2+ob1(10,10);
+  ob1.show();
+  return 0;
 }
