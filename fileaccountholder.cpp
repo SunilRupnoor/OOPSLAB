493a1bce9445479525d4e15926756c91ddf63751
SunilRupnoor <141404145+SunilRupnoor@users.noreply.github.com>
2023-08-04 14:24:50 +0530

    Update fileaccountholder.cpp

--- fileaccountholder.cpp
+++ fileaccountholder.cpp
@@ -1,77 +1,50 @@
-Design a C++ program to convert dollar to rupees, euro to rupees and pound to rupees using pure virtual functions.
-1 dollar = 54.3 Rs, 1 pound = 81.1Rs, 1 euro = 70Rs
+/*Write a C++ program to create a file to store Account holdername, account number and Balance for given number of customers. Also retrieve the values from the file and print it on
+the standard output.*/
 #include<iostream>
+#include<fstream>
 using namespace std;
-class rs
+struct account_holder
 {
-  public:
-  float rupees;
-  virtual void conv()=0;
-  void disp()
-  {
-    cout<<" is eqvivalent to "<<rupees<<" INR \n";
-  }
-};
-class doll:public rs
-{
-  float dol;
-  public:
-  void conv()
-  {
-    cout<<"Enter currncy in dollar \n";
-    cin>>dol;
-    rupees=54.3*dol;
-    cout<<" "<<dol<<" in dollar ";
-    disp();
-  }
-};
-class euro:public rs
-{
-  float er;
-  public:
-  void conv()
-  {
-    cout<<"Enter currency in Euro \n";
-    cin>>er;
-    rupees=70.2*er;
-    cout<<" "<<er<<" in euro ";
-    disp();
-  }
-};
-class pd:public rs
-{
-  float pnd;
-  public:
-  void conv()
-  {
-    cout<<"Enter currency in pound \n";
-    cin>>pnd;
-    rupees=81.1*pnd;
-    cout<<" "<<pnd<<" in pound ";
-    disp();
-  }
+  char name[20];
+  double bal;
+  long acc;
 };
 int main()
 {
-  int c;
-  doll d;
-  euro e;
-  pd p;
-  cout<<"\t\t\t Currency conversion \n";
-  while(1)
+  int n,i;
+  cout<<"Enter no. of account holders \n";
+  cin>>n;
+  struct account_holder b[10];
+  struct account_holder c[10];
+  ofstream op("TextFile2.txt");
+  if(!op)
   {
-    cout<<"1.$ to Rs. 2.Euro to Rs. 3.Pound to Rs. 4.Exit \n";
-    cin>>c;
-    switch(c)
-    {
-        case 1:d.conv();
-        break;
-  case 2:e.conv();
-  break;
-  case 3:p.conv();
-  break;
-  default:return 0;
+    cout<<"Cannot open file \n";
+    return 1;
   }
-}
-return 0;
+  for(i=0;i<n;i++)
+  {
+    cout<<"enter a/c no.,name & bal of customer : "<<i+1<<"\n";
+    cin>>b[i].acc>>b[i].name>>b[i].bal;
+    op.write((char *)&b[i],sizeof(struct account_holder));
+  }  
+  op.close();
+  ifstream ip("TextFile2.txt");
+  if(!ip)
+  {
+    cout<<"File doesn't exists \n";
+    return 1;
+  }
+  for(i=0;i<n;i++)
+  {
+    if(ip)
+    {
+      ip.read((char *)&c[i],sizeof(struct account_holder));
+      cout<<"A/c no : "<<c[i].acc<<"\n";
+      cout<<"Name : "<<c[i].name<<"\n";
+      cout<<"Balance (in INR) : "<<c[i].bal<<"\n";
+    }
+  }
+  ip.close();
+  return 0;
 }
